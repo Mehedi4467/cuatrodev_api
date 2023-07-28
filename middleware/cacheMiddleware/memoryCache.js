2f30e86021e45d09b5577383c891ac24bba90e7e
Mehedi Hassan <mehedihassan4467@gmail.com>
2023-07-28 10:42:52 +0600

    cache time change

--- middleware/cacheMiddleware/memoryCache.js
+++ middleware/cacheMiddleware/memoryCache.js
@@ -4,14 +4,15 @@ export const memoryCacheMiddleware = (req, res, next) => {
   const cachedResponse = cache.get(key);
 
   if (cachedResponse) {
-    console.log('this catch response');
+    console.log('this catch response',cachedResponse);
     res.send(JSON.parse(cachedResponse));
     return 0;
   }
 
   res.sendResponse = res.send;
   res.send = (body) => {
-    cache.put(key, body, 86400 * 1000); // Cache duration in milliseconds
+    // cache.put(key, body, 86400 * 1000); 
+    cache.put(key, body, 60 * 1000); 
     res.sendResponse(body);
   };
 
